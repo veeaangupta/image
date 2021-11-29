f7e670b2ff50d2233a07d82fee0513a6aa8f8bbb
veeaan <95080000+veeaangupta@users.noreply.github.com>
2021-11-29 09:55:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function showpeppa(){
+  document.getElementById("peppa").style.display='block'                                                                                                        
+}
+function showbean(){
+  document.getElementById("bean").style.display='block'                                                                                                        
+}
+function showben(){
+  document.getElementById("ben").style.display='block'                                                                                                        
+}
+function remove (){
+  document.getElementById("peppa").style.display='none' 
+   document.getElementById("bean").style.display='none'
+   document.getElementById("ben").style.display='none'      
+}
